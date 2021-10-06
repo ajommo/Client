25a685eafa458a741e86147274aed10d293a1c83
ajommo <ajommorpg@gmail.com>
2021-10-06 21:19:02 +0200

    Creating a basic setup with a player and a camera.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Player;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
